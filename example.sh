d67b012f594969bcfee5fd79352334610005704a
Tim Churchard <tim.churchard@iotic-labs.com>
2020-08-03 10:22:20 +0100

    0.20.1

--- example.sh
+++ example.sh
@@ -3,7 +3,7 @@
 docker stop bitcoind
 docker rm bitcoind
 
-docker pull timchurchard/docker-bitcoin:0.20.0
+docker pull timchurchard/docker-bitcoin:0.20.1
 docker image prune -f
 
 docker create \
@@ -11,7 +11,7 @@ docker create \
     -p 127.0.0.1:8332:8332 \
     -p 8333:8333 \
     -v /z-perf/solo.miner/bitcoind_data:/data \
-    timchurchard/docker-bitcoin:0.20.0
+    timchurchard/docker-bitcoin:0.20.1
 
 # Don't restart plex on boot because encrypted drives not mounted!
 docker update --restart=no bitcoind
